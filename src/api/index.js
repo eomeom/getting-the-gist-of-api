d9a8b2821f24bf9ad4c98cf27dfd0db8395bffc0
eomeom <49392319+eomeom@users.noreply.github.com>
2020-05-13 00:52:19 +0200

    Access Control fixed

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 // functions that could fetch data that we need
 import axios from 'axios';
 
-const url = 'https://covid19.mathdro.id/api/';
+const url = 'https://cors-anywhere.herokuapp.com/https://covid19.mathdro.id/api/';
 
 // // this will let us view all the data from the api
 // // "export" enables us to use the "fetchData" function from other files
